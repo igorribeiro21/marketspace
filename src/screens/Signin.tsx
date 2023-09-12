d3137544895beb6bfbc54edd59218c0b4a2d2772
igor.ribeiro <igorapalvesribeiro@gmail.com>
2023-09-11 22:05:26 -0300

    screens - feat: created a little more of the login screen

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,21 +1,75 @@
-import { VStack, ScrollView, Image, Text } from 'native-base';
-import LogoImg from '@assets/logo.jpg'
+import { VStack, ScrollView, Image, Text, Center, Input } from 'native-base';
+import LogoImg from '@assets/logo.jpg';
+import { useForm, Controller } from 'react-hook-form';
+
+type FormData = {
+    email: string;
+    password: string;
+}
 
 export function Signin() {
+    const { control, handleSubmit, formState: { errors } } = useForm<FormData>()
+
     return (
-        <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
-            <VStack
-                flex={1}
-                px={10}
-                pb={16}
-            >                
-                <Image
-                    source={LogoImg}
-                    defaultSource={LogoImg}
-                    alt='logo'
-                    resizeMode='contain'
-                    position='absolute'
-                />
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+            <VStack>
+                <Center>
+                    <VStack
+                        mt={10}
+                        alignItems='center'
+                    >
+                        <Image
+                            source={LogoImg}
+                            defaultSource={LogoImg}
+                            alt='logo'
+                            resizeMode='contain'
+                        />
+                        <Text fontWeight='bold' fontSize='3xl' lineHeight='xs'>
+                            marketspace
+                        </Text>
+                        <Text color='blueGray.500'>
+                            Seu espaço de compra e venda
+                        </Text>
+                    </VStack>
+                </Center>
+            </VStack>
+
+            <VStack mt={10}>
+                <Center>
+                    <Text fontWeight='semibold' color='blueGray.500'>
+                        Acesse sua conta
+                    </Text>
+                    <Controller
+                        control={control}
+                        name='email'
+                        render={({ field: { onChange } }) => (
+                            <Input
+                                mt={4}
+                                placeholder='E-mail'
+                                keyboardType='email-address'
+                                autoCapitalize='none'
+                                onChangeText={onChange}
+                                ml={10}
+                                mr={10}                                
+                            />
+                        )}
+                    />
+
+                    <Controller
+                        control={control}
+                        name='password'
+                        render={({ field: { onChange } }) => (
+                            <Input                                
+                                placeholder='Senha'
+                                secureTextEntry
+                                onChangeText={onChange}
+                                ml={10}
+                                mr={10}   
+                                mt={4}                             
+                            />
+                        )}
+                    />
+                </Center>
             </VStack>
         </ScrollView>
     )
